ee576e4d092f9e8053e30d9058e2008bee3c398e
Ilya <gremitskih-im@ek.apress.ru>
2017-07-14 11:53:11 +0500

    Added main Files

---
+++ js/ru-ru.js
@@ -0,0 +1,27 @@
+(function($) {
+
+  /**
+   * FlipClock Russian Language Pack
+   *
+   * This class will used to translate tokens into the Russian language.
+   *
+   */
+
+  FlipClock.Lang.Russian = {
+
+    'years'   : 'Лет',
+    'months'  : 'Месяцев',
+    'days'    : 'Дней',
+    'hours'   : 'Часов',
+    'minutes' : 'Минут',
+    'seconds' : 'Секунд'
+
+  };
+
+  /* Create various aliases for convenience */
+
+  FlipClock.Lang['ru']      = FlipClock.Lang.Russian;
+  FlipClock.Lang['ru-ru']   = FlipClock.Lang.Russian;
+  FlipClock.Lang['russian']  = FlipClock.Lang.Russian;
+
+}(jQuery));
